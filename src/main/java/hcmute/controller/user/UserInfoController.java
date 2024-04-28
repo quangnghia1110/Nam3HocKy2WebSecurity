7052076262758c851de172912f48dbb7dc32e387
Ngo Quang Nghia <21110559@st.hcmute.edu.vn>
2024-04-28 15:26:46 +0700

    test

--- src/main/java/hcmute/controller/user/UserInfoController.java
+++ src/main/java/hcmute/controller/user/UserInfoController.java
@@ -4,8 +4,9 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -25,45 +26,52 @@ import hcmute.service.IUserService;
 @RequestMapping("user_infor")
 public class UserInfoController {
 
-	@Autowired
-	IUserService userService;
+    @Autowired
+    IUserService userService;
 
-	@GetMapping("/{user_id}")
-	public String viewuser(ModelMap model, @PathVariable("user_id") Integer userId) {
-		Optional<UserEntity> opt = userService.findById(userId);
+    @GetMapping("/{user_id}")
+    public String viewuser(ModelMap model, @PathVariable("user_id") Integer userId) {
+        Optional<UserEntity> opt = userService.findById(userId);
+        
+        // Bảo mật web
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUsername = authentication.getName();
+        if (opt.isPresent()) {
+            UserEntity user = opt.get();
+            // Kiểm tra xem người dùng đã xác thực có phải là người dùng được yêu cầu không
+            if (user.getUsername().equals(loggedInUsername)) {
+                model.addAttribute("user", user);
+                return "user/user_infor";
+            } else {
+                // Nếu người dùng đã xác thực không phải là người dùng được yêu cầu
+                model.addAttribute("message", "Bạn không có quyền truy cập vào thông tin của người dùng này.");
+                return "user/error";
+            }
+        } else {
+            model.addAttribute("message", "Không tìm thấy thông tin khách hàng với ID: " + userId);
+            return "user/error";
+        }
+    }
 
-		if (opt.isPresent()) {
-			UserEntity user = opt.get();
-			model.addAttribute("user", user);
-			return "user/user_infor";
-		} else {
-			model.addAttribute("message", "Không tìm thấy thông tin khách hàng với ID: " + userId);
-			return "user/error";
-		}
-	}
+    @PostMapping("/edit/{user_id}")
+    public ModelAndView edit(ModelMap model, @PathVariable("user_id") Integer userId,
+            RedirectAttributes redirectAttributes, @Valid @ModelAttribute("user") UserModel user,
+            BindingResult result) {
 
-	@PostMapping("/edit/{user_id}")
-	public ModelAndView edit(ModelMap model, @PathVariable("user_id") Integer userId,
-			RedirectAttributes redirectAttributes, @Valid @ModelAttribute("user") UserModel user,
-			BindingResult result) {
+        if (user != null) {
+            UserEntity entity = userService.findById(userId).orElse(null);
+            if (entity != null) {
+                // Update user information
+                entity.setName(user.getName());
+                entity.setSurname(user.getSurname());
+                entity.setGender(user.getGender());
+                entity.setPhoneNumber(user.getPhoneNumber());
 
-
-		if (user != null) {
-			UserEntity entity = userService.findById(userId).get();
-			if (entity != null) {
-				// Cập nhật thông tin người dùng
-				entity.setName(user.getName());
-				entity.setSurname(user.getSurname());
-				entity.setGender(user.getGender());
-				entity.setPhoneNumber(user.getPhoneNumber());
-
-				userService.save(entity);
-				redirectAttributes.addFlashAttribute("message", "Thông tin đã được cập nhật thành công!");
-				redirectAttributes.addFlashAttribute("status", "success");
-			}
-		}
-		return new ModelAndView("redirect:/user_infor/" + userId);
-
-	}
-
+                userService.save(entity);
+                redirectAttributes.addFlashAttribute("message", "Thông tin đã được cập nhật thành công!");
+                redirectAttributes.addFlashAttribute("status", "success");
+            }
+        }
+        return new ModelAndView("redirect:/user_infor/" + userId);
+    }
 }
